eac707c807c55abaaf17695920cbc6c7ff86c527
Ruslan Petrov <richhomiequan94@mail.ru>
2020-11-12 12:58:41 -0500

    Started working on TC002

--- src/GroupPractice.java
+++
@@ -1,9 +0,0 @@
-public class GroupPractice {
-    public static void main(String[] args) {
-
-        System.out.println("4Tek");
-        System.out.println("Crystal is the best =)");
-        System.out.println(" Petar Tanakov is here ");
-
-    }
-}
